40a95bdea607b85758a43bfb95f3a6102af66166
Robert Kornacki <11645932+robkorn@users.noreply.github.com>
2021-01-30 19:34:14 +0100

    Removed clear from script

--- setup.sh
+++ setup.sh
@@ -3,6 +3,5 @@ cargo build --release
 cp target/release/urbit-operator-toolkit deployed
 sleep 0.5
 cd deployed
-clear
 echo "The Urbit Operator Toolkit has finished compiling and can be found in the deployed folder."
 ./urbit-operator-toolkit
