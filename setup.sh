fa0665214fbdc4d2bc6320f016f69821f4bedb07
Robert Kornacki <11645932+robkorn@users.noreply.github.com>
2021-02-23 00:26:47 +0100

    Updated setup script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 mkdir deployed
 cargo build --release
-cp target/release/urbit-operator-toolkit deployed
+cp target/release/urbit-content-archiver deployed
 sleep 0.5
 cd deployed
 echo "The Urbit Content Archiver has finished compiling and can be found in the deployed folder."
-./urbit-operator-toolkit
